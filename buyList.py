ed2d2b94cf734a6b8b5746ee8fbb7bb5de85ad53
MSHE97 <sherzod.mustafokulov@mail.ru>
2021-09-01 17:17:38 +0500

    Input parsed as objects Product

--- buyList.py
+++ buyList.py
@@ -2,28 +2,43 @@
 
 import sys
 
-class IOFile:
+class Input:
     file = sys.argv[1]
     operation = sys.argv[2]
 
-class PriceList:
-    def PriceList():
-        products = []
-    def PriceList(self, list):
-        products = list
-       
-
-
+class Product:
+    def __init__(self, a, b,):
+        self.productName = a
+        self.price = b 
+    
+    
 def main():
-    myFile = IOFile()
+    myInput = Input()
     try:
-        f = open(myFile.file)
+        f = open(myInput.file, 'r')
     except IOError:
         input("Can't find such file. Please, press enter to exit")
         return
+    
+    products = []
     for line in f:
-        print(line)
-    f.close()
+        space = line.find(' - ')
+        productName = line[:space]
+        price = float( line[ space + 3:-1] ) 
+        products.append(Product(productName, price))
+
+    # buy = PriceList()
+    # if(myFile.operation == '1'):
+    #     buy.add("Carrots - 75", f)
+
+    # if( myFile.operation == '2'):
+    #     buy.update("Onions - 50", "Onions - 30", f)
+
+
+    # f = open(myFile.file, 'r')
+    # for line in f:
+    #     print(line)
+    # f.close()
 
 if __name__ == '__main__':
     main()
